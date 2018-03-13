da75cf113cf978332f4b64717f0ae75fb69c0008
Matt <deal.sy@hotmail.com>
2018-03-13 19:03:03 +1100

    added endpoints for login and deals pages

--- app/users/views.py
+++ app/users/views.py
@@ -4,9 +4,37 @@ from jinja2 import TemplateNotFound
 
 user_blueprint = Blueprint('user_blueprint', __name__, template_folder='templates')
 
+"""
+Submit_page:
+
+returns the html page for users to submit a deal
+
+"""
 
 @user_blueprint.route('/submit')
 def submit_page():
     return render_template("submit_deals.html")
 
 
+"""
+login:
+
+returns the html page for users to login to the website
+
+"""
+
+@user_blueprint.route('/login')
+def login():
+    return render_template("login.html")
+
+
+"""
+deals_page:
+
+returns the html page for users to view current deals on the blockchain. 
+
+"""
+
+@user_blueprint.route('/deals')
+def deals_page():
+    return render_template("deals_page.html")
