39383735c122e56c1acb560fbc7859377e7be518
Scott Weston <scott@srw.id.au>
2018-03-18 17:22:10 +1100

    improved deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd ~/blockdeals
-git pull
-docker stop blockdeals
-docker rm blockdeals
-docker build -t blockdeals . && docker run --link mongo:mongodb -p 127.0.0.1:5000:8000 -d --name blockdeals blockdeals
+git pull \
+  && docker build -t blockdeals . \
+  && docker stop blockdeals \
+  && docker rm blockdeals \
+  && docker run --link mongo:mongodb -p 127.0.0.1:5000:8000 -d --name blockdeals blockdeals
