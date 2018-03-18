1e730f4743a6f835b5fba11ea561cf5f719924c7
Scott Weston <scott@srw.id.au>
2018-03-18 16:29:30 +1100

    initial db support

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import uuid
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
+db = MongoClient("mongodb://mongodb:27017").blockdeals
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    # TODO: only show non-expired deals... paginate?
+    deals = []
+    deal_cursor=db.deal.find({})
+    for deal in deal_cursor:
+        deals.append(deal)
+    print(deals)
+    return render_template('index.html', deals=deals)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.route('/deal', methods=['GET', 'POST'])
+def deal():
+    deal_form=request.form.to_dict()
+    print(db['deal'].insert(deal_form))
+    # TODO: make a pretty template
+    return 'deal posted'
--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd ~/blockdeals
 git pull
 docker stop blockdeals
 docker rm blockdeals
-docker build -t blockdeals . && docker run -p 127.0.0.1:5000:8000 -d --name blockdeals blockdeals
+docker build -t blockdeals . && docker run --link mongo:mongodb -p 127.0.0.1:5000:8000 -d --name blockdeals blockdeals
