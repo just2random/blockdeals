bb88ad4680c132d3ba8bb82b9c98c163c712ddd2
Scott Weston <scott@srw.id.au>
2018-04-09 21:14:23 +1000

    initial support for vote count

--- app/static/assets/js/blockdeals.js
+++ app/static/assets/js/blockdeals.js
@@ -1,3 +1,5 @@
+const steem = new dsteem.Client('https://api.steemit.com');
+
 $(document).ready(function() {
   $('.loginbtn').click(function() {
     window.location.replace(sc2.getLoginURL());
@@ -19,6 +21,21 @@ $(document).ready(function() {
     $.each(data, function(index, country_code) {
       $('#country_dropdown').append('<li><a class="truncate" href="/country/' + country_code + '"><div class="country-select"><div class="flag ' + country_code + '"></div></div> ' + countries[country_code] + '</a></li>');
       $('#mobile_freebies').after('<li><a class="truncate" href="/country/' + country_code + '"><div class="country-select"><div class="flag ' + country_code + '"></div></div> ' + countries[country_code] + '</a></li>');
+    });
+  });
+
+  $('.votes').each(function(i, e) {
+    steem.database.getDiscussions('active', {select_authors: [$(e).data("author")], start_author: $(e).data("author"), start_permlink: $(e).data("permlink"), limit: 1}).then(function(d) {
+      if (d[0].author == $(e).data("author") && d[0].permlink == $(e).data("permlink")) {
+        $(e).html("<i class='fas fa-thumbs-up fa-fw'></i> " + d[0]['active_votes'].length);
+      }
+      else {
+        console.log("failed to find post: ", $(e).data("author"), $(e).data("permlink"));
+        $(e).html("");
+      }
+    }).catch(function(err) {
+      console.log("failed to find post: ", $(e).data("author"), $(e).data("permlink"));
+      $(e).html("");
     });
   });
 });
