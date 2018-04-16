43f7ff4f3b4252cb9fc88bd069c1b0191844ed31
Scott Weston <scott@srw.id.au>
2018-04-16 13:21:55 +1000

    fix: navbar layout for new users

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git pull \
   && docker build -t blockdeals . \
   && docker stop blockdeals \
   && docker rm blockdeals \
-  && docker run -v /blockdeals.cfg:/blockdeals/blockdeals.cfg:ro --link=mongo:mongodb -p 127.0.0.1:5000:8000 -d --name=blockdeals --restart=always blockdeals
+  && docker run -v /blockdeals.cfg:/blockdeals/blockdeals.cfg:ro --link=mongo:mongodb -p 127.0.0.1:5000:8000 -d --name=blockdeals --restart=unless-stopped blockdeals
