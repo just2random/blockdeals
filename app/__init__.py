97e929ebb216f50bba000cf9cb77b3779beb9dd2
Scott Weston <scott@srw.id.au>
2018-04-22 20:39:47 +1000

    fix: tz issues losing a day

--- app/__init__.py
+++ app/__init__.py
@@ -65,6 +65,8 @@ def _jinja2_filter_expires_time(date, fmt=None):
     native = date.replace(tzinfo=None)
     days = (native-date.today()).days
     if days < 0:
+        return "today!"
+    elif days < -1:
         return "{} day{} ago".format(abs(days), '' if abs(days) == 1 else 's')
     elif days <= 2:
         return "soon"
