fb1c1ad4c0af0cdc5a196a6ea69896db2f035392
just2random <36524020+just2random@users.noreply.github.com>
2018-06-27 18:30:40 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd ~/blockdeals
+cd ~/fomodeals
 git pull \
-  && docker build -t blockdeals . \
-  && docker stop blockdeals \
-  && docker rm blockdeals \
-  && docker run -v /blockdeals.cfg:/blockdeals/blockdeals.cfg:ro --link=mongo:mongodb -p 127.0.0.1:5000:8000 -d --name=blockdeals --restart=unless-stopped blockdeals
+  && docker build -t fomodeals . \
+  && docker stop fomodeals \
+  && docker rm fomodeals \
+  && docker run -v /fomodeals.cfg:/fomodeals/fomodeals.cfg:ro --link=mongo:mongodb -p 127.0.0.1:5000:8000 -d --name=fomodeals --restart=unless-stopped fomodeals
