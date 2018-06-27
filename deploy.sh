c09237262e638d9f0b24b16e3f409825d0ad1953
just2random <36524020+just2random@users.noreply.github.com>
2018-06-27 18:55:16 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ~/fomodeals
-git pull \
+git pull https://github.com/just2random/fomodeals.git \
   && docker build -t fomodeals . \
   && docker stop fomodeals \
   && docker rm fomodeals \
-  && docker run -v /fomodeals.cfg:/fomodeals/fomodeals.cfg:ro --link=mongo:mongodb -p 127.0.0.1:5000:8000 -d --name=fomodeals --restart=unless-stopped fomodeals
+  && docker run -v /fomodeals.cfg:/fomodeals/fomodeals.cfg:ro --link=fomomongo:mongodb -p 127.0.0.1:5000:8000 -d --name=fomodeals --restart=unless-stopped fomodeals
